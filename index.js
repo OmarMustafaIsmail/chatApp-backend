ab4eb93f8456f633c77c179c47731dfa3ffd1ff1
OmarMustafaIsmail <66802392+OmarMustafaIsmail@users.noreply.github.com>
2022-02-07 17:02:38 +0200

    receive message from front_end

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ var io = require("socket.io")(server);
 app.use(express.json());
 
 
-
+var clients = {};
 
 io.on("connection",(socket)=>{
     console.log("connected");
     console.log(socket.id + "Joined The chat");
-    socket.on("/test" , (msg)=>{
-        console.log(msg)
+    socket.on("/signin" , (id)=>{
+        console.log(id)
+        clients[id]=socket;
+        console.log(clients)
+    })
+    socket.on("message",(msg)=>{
+        console.log(msg);
     })
 })
 
