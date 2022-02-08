a5d8a54966c49431dae1c744acd70e7f3be275d0
OmarMustafaIsmail <66802392+OmarMustafaIsmail@users.noreply.github.com>
2022-02-08 15:30:31 +0200

    sending and recieving messages

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ io.on("connection",(socket)=>{
     })
     socket.on("message",(msg)=>{
         console.log(msg);
+        let targetId = msg.targetId;
+        if(clients[targetId])
+        clients[targetId].emit("message",msg);
+        
     })
 })
 
